d7b190517cbeb0a34bac1cd63f505c06a54773d8
Sherin Thomas <sherinct@live.com>
2019-03-29 00:51:49 +0530

    Front end of ChatBot example (#1)
    * CORS issue
    
    * chatbot fronte nd

--- python_client/chatbot/app.py
+++ python_client/chatbot/app.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
 import utils
 import redis_db
 
@@ -9,6 +10,7 @@ db_port = os.environ.get('REDIS_PORT') or 6379
 
 
 app = Flask(__name__)
+CORS(app)
 redis_db = redis_db.DB(host=db_host, port=db_port)
 redis_db.initiate()
 
@@ -16,14 +18,24 @@ redis_db.initiate()
 @app.route('/chat', methods=['POST'])
 def chat():
     req_data = request.get_json()
-    message = get_bot_reply(req_data['message'])
-    return jsonify(response=message)
+    message = utils.normalize_string((req_data['message']))
+    try:
+        indices = utils.get_batched_indices(message)
+    except KeyError:
+        reply = "I did not understand your language!!, check the spelling perhaps"
+    else:
+        numpy_array = utils.list2numpy(indices)
+        length = utils.get_length(numpy_array)
+        reply = redis_db.process(numpy_array, length)
+    resp = jsonify(reply=reply)
+    return resp
 
 
-def get_bot_reply(message):
-    message = utils.normalize_string(message)
-    indices = utils.get_batched_indices(message)
-    numpy_array = utils.list2numpy(indices)
-    length = utils.get_length(numpy_array)
-    reply = redis_db.process(numpy_array, length)
-    return reply
+@app.route('/<path:filepath>')
+def ui_components(filepath):
+    return send_from_directory('static', filepath)
+
+
+@app.route('/')
+def ui():
+    return send_from_directory('static', 'index.html')
