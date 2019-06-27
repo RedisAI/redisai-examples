ab316228fa57822bcdd84e8ac208405e22b86924
hhsecond <sherin@tensorwerk.com>
2019-06-27 12:55:13 +0530

    spark python ex

--- python_client/spark_decisiontree.py
+++ python_client/spark_decisiontree.py
@@ -1,15 +1,15 @@
+import numpy as np
 import redisai as rai
 from redisai.model import load_model
-model = load_model("../models/spark/decisiontree_with_pipeline/dtree.onnx")
+
+model = load_model("../models/spark/decisiontree_with_pipeline/spark.onnx")
 
 con = rai.Client()
 con.modelset("spark_model", rai.Backend.onnx, rai.Device.cpu, model)
-label = [1.0]
-features = [1.0, 1.0, 1.0, 1.0, 1.0]
-ltensor = rai.Tensor.scalar(rai.DType.float, *label)
-ftensor = rai.Tensor.scalar(rai.DType.float, *features)
-con.tensorset("linput", ltensor)
-con.tensorset("finput", ftensor)
-con.modelrun("spark_model", ["linput", "finput"], ["output1", "out2", "out3"])
+features = np.array([[0., 0., 0., 0., 0.], [0., 0., 0., 17., 206.]], dtype=np.float32)
+breakpoint()
+tensor = rai.BlobTensor.from_numpy(features)
+con.tensorset("input", tensor)
+con.modelrun("spark_model", ["input"], ["output"])
 outtensor = con.tensorget("output", as_type=rai.BlobTensor)
 print(outtensor.to_numpy())
