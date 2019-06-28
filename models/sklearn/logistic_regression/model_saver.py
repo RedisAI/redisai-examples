368101e965ca2e1a88a08a71b8db0c4bea2ff45d
hhsecond <sherin@tensorwerk.com>
2019-06-28 13:13:25 +0530

    logistic regression sklearn

---
+++ models/sklearn/logistic_regression/model_saver.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from redisai.model import save_sklearn
+from redisai.model import onnx_utils
+from redisai import DType
+
+
+iris = load_iris()
+X, y = iris.data, iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+print('Input shape:', X_test.shape)
+
+logistic = LogisticRegression(solver='liblinear', multi_class='auto')
+logistic.fit(X_train, y_train)
+
+
+initial_type = onnx_utils.get_tensortype(
+    node_name='float_input', dtype=DType.float32, shape=(1, 4))
+
+save_sklearn(logistic, 'logistic.onnx', initial_types=[initial_type])
---
+++ python_client/sklearn_logistic_regression.py
@@ -0,0 +1,19 @@
+import numpy as np
+import redisai as rai
+from redisai.model import load_model
+
+model = load_model("../models/sklearn/logistic_regression/logistic.onnx")
+
+con = rai.Client()
+con.modelset("sklearn_model", rai.Backend.onnx, rai.Device.cpu, model)
+
+dummydata = np.array([[6.9, 3.1, 5.4, 2.1]], dtype=np.float32)
+tensor = rai.BlobTensor.from_numpy(dummydata)
+con.tensorset("input", tensor)
+
+# dummy output because by default sklearn logistic regression outputs
+# value and probability. Since RedisAI doesn't support specifying required
+# outputs now, we need to keep placeholders for all the default outputs.
+con.modelrun("sklearn_model", ["input"], ["output", "dummy"])
+outtensor = con.tensorget("output", as_type=rai.BlobTensor)
+print(f" Output class: {outtensor.to_numpy().item()}")
