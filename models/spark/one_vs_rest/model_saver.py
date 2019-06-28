65735c683988306e4193510df00b5666eec0e810
hhsecond <sherin@tensorwerk.com>
2019-06-28 09:44:57 +0530

    one vs rest spark example

---
+++ models/spark/one_vs_rest/model_saver.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.ml.classification import LogisticRegression, OneVsRest
+from redisai.model import save_sparkml
+from redisai.model import onnx_utils
+from redisai import DType
+
+
+executable = sys.executable
+os.environ["SPARK_HOME"] = pyspark.__path__[0]
+os.environ["PYSPARK_PYTHON"] = executable
+os.environ["PYSPARK_DRIVER_PYTHON"] = executable
+spark = SparkSession.builder.appName("redisai_trial").getOrCreate()
+
+data = spark.read.format("libsvm").load('multiclass_classification_data.txt')
+lr = LogisticRegression(maxIter=100, tol=0.0001, regParam=0.01)
+ovr = OneVsRest(classifier=lr)
+model = ovr.fit(data)
+feature_count = data.first()[1].size
+tensor_types = onnx_utils.get_tensortype(
+    node_name='features', dtype=DType.float32, shape=(1, feature_count))
+save_sparkml(model, 'spark.onnx', initial_types=[tensor_types])
