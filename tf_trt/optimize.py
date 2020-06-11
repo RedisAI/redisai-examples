e38edd1003e86906e30a392d63d4f5a196a0ed57
filipecosta90 <filipecosta.90@gmail.com>
2020-06-11 15:15:03 +0100

    [wip] wip on having a graphdef that is compatible with trt

--- tf_trt/optimize.py
+++ tf_trt/optimize.py
@@ -21,30 +21,55 @@ def check_tensor_core_gpu_present():
             
 print("Tensor Core GPU Present:", check_tensor_core_gpu_present()) 
 
-DATA_DIR = '../models/tensorflow/imagenet/'
-MODEL = 'resnet50'
-TRT_SUFFIX = '_fp16_trt'
 
-output_names = ['output']
+import tensorflow as tf
+import tensorflow_hub as hub
 
-print("------------- Load frozen graph from disk -------------")
-with tf.gfile.GFile(DATA_DIR + MODEL + '.pb', "rb") as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
+var_converter = tf.compat.v1.graph_util.convert_variables_to_constants
+url = 'https://tfhub.dev/google/imagenet/resnet_v2_50/classification/1'
+images = tf.placeholder(tf.float32, shape=(1, 224, 224, 3), name='images')
+module = hub.Module(url)
+print(module.get_signature_names())
+print(module.get_output_info_dict())
+logits = module(images)
+logits = tf.identity(logits, 'output')
+with tf.Session() as sess:
+    sess.run([tf.global_variables_initializer()])
+    graph_def = sess.graph_def
 
-print("------------- Optimize the model with TensorRT -------------")
-    # max_batch_size=1,
-    # max_workspace_size_bytes=1 << 26,
-    # precision_mode='FP16',
-    # minimum_segment_size=2
-trt_graph = trt.create_inference_graph(
-    input_graph_def=graph_def,
-    outputs=output_names
+    # clearing device information
+    for node in graph_def.node:
+        node.device = ""
+    frozen = tf.graph_util.convert_variables_to_constants(sess, graph_def, ['output'])
+    trt_graph = trt.create_inference_graph(
+    input_graph_def=frozen,
+    outputs=['output']
+    )
 
-)
+# DATA_DIR = '../models/tensorflow/imagenet/'
+# MODEL = 'resnet50'
+# TRT_SUFFIX = '_fp16_trt'
 
-print("------------- Write optimized model to the file -------------")
-with open(DATA_DIR + MODEL + TRT_SUFFIX + '.pb', 'wb') as f:
-    f.write(trt_graph.SerializeToString())
+# output_names = ['output']
 
-print("------------- DONE! -------------")
+# print("------------- Load frozen graph from disk -------------")
+# with tf.gfile.GFile(DATA_DIR + MODEL + '.pb', "rb") as f:
+#     graph_def = tf.GraphDef()
+#     graph_def.ParseFromString(f.read())
+
+# print("------------- Optimize the model with TensorRT -------------")
+#     # max_batch_size=1,
+#     # max_workspace_size_bytes=1 << 26,
+#     # precision_mode='FP16',
+#     # minimum_segment_size=2
+# trt_graph = trt.create_inference_graph(
+#     input_graph_def=graph_def,
+#     outputs=output_names
+
+# )
+
+# print("------------- Write optimized model to the file -------------")
+# with open(DATA_DIR + MODEL + TRT_SUFFIX + '.pb', 'wb') as f:
+#     f.write(trt_graph.SerializeToString())
+
+# print("------------- DONE! -------------")
