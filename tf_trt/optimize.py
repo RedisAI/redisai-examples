3ed0018878edcefc287c1573a8d0e450b44a7914
filipecosta90 <filipecosta.90@gmail.com>
2020-06-11 14:46:51 +0100

    [wip] updated deprecta warnings

--- tf_trt/optimize.py
+++ tf_trt/optimize.py
@@ -1,9 +1,26 @@
+## on aws dl ami source activate tensorflow_p36
+
 import tensorflow as tf
 import os
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
+from tensorflow.python.client import device_lib 
 
 tf_model_path = '../models/tensorflow/imagenet/resnet50.pb'
 tf_trt_model_path = '../models/tensorflow-trt/imagenet/tf-trt-resnet50.pb'
+
+def check_tensor_core_gpu_present(): 
+    result = False
+    local_device_protos = device_lib.list_local_devices() 
+    for line in local_device_protos: 
+        if "compute capability" in str(line): 
+            compute_capability = float(line.physical_device_desc.split("compute capability: ")[-1]) 
+            if compute_capability>=7.0: 
+                result = True 
+    
+    return result 
+            
+print("Tensor Core GPU Present:", check_tensor_core_gpu_present()) 
+
 
 def load_model(path):
     with tf.gfile.GFile(path, "rb") as f:
