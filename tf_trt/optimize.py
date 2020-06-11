f1f560e9ce702711624696c5c248230cdd44ddcb
filipecosta90 <filipecosta.90@gmail.com>
2020-06-11 14:36:57 +0100

    [wip] updated deprecta warnings

--- tf_trt/optimize.py
+++ tf_trt/optimize.py
@@ -5,9 +5,16 @@ from tensorflow.python.compiler.tensorrt import trt_convert as trt
 tf_model_path = '../models/tensorflow/imagenet/resnet50.pb'
 tf_trt_model_path = '../models/tensorflow-trt/imagenet/tf-trt-resnet50.pb'
 
-graph_def = tf.compat.v1.GraphDef()
-with tf.io.gfile.GFile(tf_model_path, 'rb') as f:
-    graph_def.ParseFromString(f.read())
+def load_model(path):
+    with tf.gfile.GFile(path, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    with tf.Graph().as_default() as graph:
+        tf.import_graph_def(graph_def, name="")
+    return graph
+
+graph_def = load_model(tf_model_path)
 
 converter = trt.TrtGraphConverter(input_graph_def=graph_def)
 graph_def = converter.convert()
