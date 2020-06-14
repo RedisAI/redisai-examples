20e376cc6a5734650319ea39c7fcb18248d2a45c
filipecosta90 <filipecosta.90@gmail.com>
2020-06-14 22:27:37 +0100

    [add] improved optimize python scripts

---
+++ tf_trt/mobilenet_v1_100_224/optimize.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import ml2rt
+import sys
+# on aws dl ami source activate tensorflow_p36
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import os
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
+from tensorflow.python.client import device_lib
+var_converter = tf.compat.v1.graph_util.convert_variables_to_constants
+
+tf_trt_model_path = '../models/tensorflow/mobilenet/mobilenet_v1_100_224_fp16_trt.pb'
+
+url = 'https://tfhub.dev/google/imagenet/mobilenet_v1_100_224/quantops/classification/3'
+
+gpu_available = tf.test.is_gpu_available(
+    cuda_only=True, min_cuda_compute_capability=None
+)
+
+if gpu_available is False:
+    print("No CUDA GPUs found. Exiting...")
+    sys.exit(1)
+
+module = hub.Module(url)
+batch_size = 1
+height, width = hub.get_expected_image_size(module)
+input_var = 'input'
+output_var = 'MobilenetV1/Predictions/Reshape_1'
+images = tf.placeholder(tf.float32, shape=(batch_size, height, width, 3), name=input_var)
+print(module.get_signature_names())
+print(module.get_output_info_dict())
+logits = module(images)
+logits = tf.identity(logits, output_var)
+with tf.Session() as sess:
+    sess.run([tf.global_variables_initializer()])
+    graph_def = sess.graph_def
+
+    # clearing device information
+    for node in graph_def.node:
+        node.device = ""
+    frozen = tf.graph_util.convert_variables_to_constants(
+        sess, graph_def, [output_var])
+
+    print("Optimizing the model with TensorRT")
+
+    converter = trt.TrtGraphConverter(
+        input_graph_def=frozen,
+        nodes_blacklist=[output_var],
+        precision_mode='FP16',
+    )
+
+    frozen_optimized = converter.convert()
+    directory = os.path.dirname(tf_trt_model_path)
+    file = os.path.basename(tf_trt_model_path)
+    tf.io.write_graph(frozen_optimized, directory, file, as_text=False)
+
