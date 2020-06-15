da0799d0c0b9576ce528ea34f142f689dab9f616
filipecosta90 <filipecosta.90@gmail.com>
2020-06-15 17:42:30 +0100

    [add] added requirements to python client examples. improved mobilenet scripts

--- python_client/cli.py
+++ python_client/cli.py
@@ -1,8 +1,13 @@
 import argparse
+import sys 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--gpu', action="store_true", default=False)
 parser.add_argument('--host', action="store", default='localhost')
 parser.add_argument('--port', action="store", default=6379, type=int)
-
+parser.add_argument('--input-shape', default="NxHxWxC", type=str)
 arguments = parser.parse_args()
+
+if arguments.input_shape != "NxHxWxC" and arguments.input_shape != "NxHxWxC":
+    print("--input-shape is either NxHxWxC or NxCxHxW. Exiting...")
+    sys.exit(1)
--- python_client/tensorflow_mobilenet_v1_224.py
+++ python_client/tensorflow_mobilenet_v1_224.py
@@ -5,14 +5,11 @@ from ml2rt import load_model, load_script
 from skimage import io
 from cli import arguments
 
-if arguments.gpu:
-    device = 'gpu'
-else:
-    device = 'cpu'
+device = 'gpu' if arguments.gpu else 'cpu'
 
 con = rai.Client(host=arguments.host, port=arguments.port)
-
-tf_model_path = '../models/tensorflow/mobilenet/mobilenet_v1_100_224.pb'
+model_key_name = 'mobilenet_v1_100_224_{device}_{input_shape}'.format(  device=device, input_shape=arguments.input_shape)
+tf_model_path = '../models/tensorflow/mobilenet/mobilenet_v1_100_224_{device}_{input_shape}.pb'.format(  device=device, input_shape=arguments.input_shape)
 script_path = '../models/tensorflow/resnet50/data_processing_script.txt'
 img_path = '../data/cat.jpg'
 input_var = 'input'
@@ -26,15 +23,17 @@ script = load_script(script_path)
 
 
 out1 = con.modelset(
-    'mobilenet_224', 'tf', device,
+    model_key_name, 'tf', device,
     inputs=[input_var], outputs=[output_var], data=tf_model)
 out2 = con.scriptset('imagenet_script', device, script)
 a = time.time()
 con.tensorset('image', image)
 out4 = con.scriptrun('imagenet_script', 'pre_process_3ch', 'image', 'temp1')
-out5 = con.modelrun('mobilenet_224', 'temp1', 'temp2')
+out5 = con.modelrun(model_key_name, 'temp1', 'temp2')
 out6 = con.scriptrun('imagenet_script', 'post_process', 'temp2', 'out')
 final = con.tensorget('out')
+took = time.time() - a
 ind = final.item()
 print(ind, class_idx[str(ind)])
-print(time.time() - a)
+print("Took {}".format(took))
+
