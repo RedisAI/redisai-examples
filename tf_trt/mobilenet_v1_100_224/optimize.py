d69e05b6a88a0c60bfb996bdbc0b6d2545e39b3b
filipecosta90 <filipecosta.90@gmail.com>
2020-06-17 11:39:39 +0100

    [add] moving mobilenet export to tensorflow 2.2

--- tf_trt/mobilenet_v1_100_224/optimize.py
+++ tf_trt/mobilenet_v1_100_224/optimize.py
@@ -1,28 +1,28 @@
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
+import logging
+import sys
+import tensorflow_hub as hub
+import tensorflow as tf
 import os
 
 #  restricting execution to a specific device or set of devices for debugging and testing
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-import tensorflow as tf
-import tensorflow_hub as hub
-import sys
-import logging
 # on aws dl ami source activate tensorflow_p36
-from tensorflow.python.compiler.tensorrt import trt_convert as trt
 
-from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
 compiled_version = trt.wrap_py_utils.get_linked_tensorrt_version()
 loaded_version = trt.wrap_py_utils.get_loaded_tensorrt_version()
-print("$$$$$$$$$$$$$$$$$$$$$$$$ Linked TensorRT version: %s" % str(compiled_version))
-print("$$$$$$$$$$$$$$$$$$$$$$$$ Loaded TensorRT version: %s" % str(loaded_version))
+print("$$$$$$$$$$$$$$$$$$$$$$$$ Linked TensorRT version: %s" %
+      str(compiled_version))
+print("$$$$$$$$$$$$$$$$$$$$$$$$ Loaded TensorRT version: %s" %
+      str(loaded_version))
 
 trt._check_trt_version_compatibility()
 
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 
 tf_trt_model_path = '../../models/tensorflow/mobilenet/mobilenet_v1_100_224_gpu_NxHxWxC_fp16_trt.pb'
-
-url = 'https://tfhub.dev/google/imagenet/mobilenet_v1_100_224/quantops/classification/3'
 
 gpu_available = tf.test.is_gpu_available(
     cuda_only=True, min_cuda_compute_capability=None
@@ -33,37 +33,33 @@ if gpu_available is False:
     print("No CUDA GPUs found. Exiting...")
     sys.exit(1)
 
-module = hub.Module(url)
+full_model = tf.keras.Sequential([
+    hub.KerasLayer(
+        "https://tfhub.dev/google/imagenet/mobilenet_v1_100_224/classification/4")
+])
+full_model.build([None, 224, 224, 3])  # Batch input shape.
+
 batch_size = 1
-height, width = hub.get_expected_image_size(module)
+height, width = 224, 224
 input_var = 'input'
 output_var = 'MobilenetV1/Predictions/Reshape_1'
-images = tf.placeholder(tf.float32, shape=(batch_size, height, width, 3), name=input_var)
-logits = module(images)
-logits = tf.identity(logits, output_var)
-
 
 conversion_params = trt.DEFAULT_TRT_CONVERSION_PARAMS._replace(
     precision_mode=trt.TrtPrecisionMode.FP16)
 
-with tf.Session() as sess:
-    sess.run([tf.global_variables_initializer()])
-    graph_def = sess.graph_def
+frozen_func = convert_variables_to_constants_v2(full_model)
+frozen_func.graph.as_graph_def()
 
-    # clearing device information
-    for node in graph_def.node:
-        node.device = ""
-    frozen = convert_variables_to_constants_v2(graph_def)
-    frozen.graph.as_graph_def()
+print("Optimizing the model with TensorRT")
 
-    print("Optimizing the model with TensorRT")
-
-    converter = trt.TrtGraphConverterV2(
-    input_graph_def=frozen,
+converter = trt.TrtGraphConverterV2(
+    input_graph_def=frozen_func,
     conversion_params=conversion_params)
-    converter.convert()
 
-    frozen_optimized = converter.convert()
-    directory = os.path.dirname(tf_trt_model_path)
-    file = os.path.basename(tf_trt_model_path)
-    tf.io.write_graph(frozen_optimized, directory, file, as_text=False)
+frozen_optimized = converter.convert()
+directory = os.path.dirname(tf_trt_model_path)
+file = os.path.basename(tf_trt_model_path)
+tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
+                  logdir=".",
+                  name=file,
+                  as_text=False)
